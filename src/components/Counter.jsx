c349af65d51c57304b1eb02b4ea9b5150465c75a
rmkofficial <kmert0@outlook.com>
2024-03-20 14:52:36 +0300

    Add toggle functionality and increase counter by 5

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,20 +2,29 @@ import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
   const incrementHandler = () => {
     dispatch({ type: "INCREMENT" });
   };
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 5 });
+  };
   const decrementHandler = () => {
     dispatch({ type: "DECREMENT" });
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
   };
 
   return (
     <main>
       <h1>Redux Counter</h1>
-      <b> {counter} </b> <br />
-      <button onClick={incrementHandler}>Increment</button>
-      <button onClick={decrementHandler}>Decrement</button>
+      <b> {showCounter && counter} </b> <br />
+      <button onClick={incrementHandler}>Increment</button> <b />
+      <button onClick={increaseHandler}>Increment by 5</button> <b />
+      <button onClick={decrementHandler}>Decrement</button> <b/>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
+// store.js
+
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 2, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
     return {
+      ...state,
       counter: state.counter + 1,
+    };
+  }
+  if (action.type === "INCREASE") {
+    return {
+      ...state,
+      counter: state.counter + action.amount,
     };
   }
   if (action.type === "DECREMENT") {
     return {
+      ...state,
       counter: state.counter - 1,
+    };
+  }
+  if (action.type === "TOGGLE") {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
     };
   }
 
